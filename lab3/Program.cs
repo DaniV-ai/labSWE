d7e27ee37a771196c64332029c49a1812e0563ca
Dan Vaskevych <59373110+DaniV-ai@users.noreply.github.com>
2024-09-04 12:47:23 +0300

    implement-first-principle

--- lab3/Program.cs
+++ lab3/Program.cs
@@ -1,1 +1,52 @@
-﻿
+﻿using System;
+
+
+// Single Responsibility principle
+namespace lab3{
+    public static class lab3{
+        static int Main(){
+            
+            Car car = new Car();
+            car.drive();
+
+
+            PassengerCar passengerCar = new PassengerCar();
+            Lorry lorry = new Lorry();
+            ArmoredTruck armoredTruck = new ArmoredTruck();
+
+            passengerCar.drive();
+            lorry.drive();
+            armoredTruck.drive();
+
+            return 0;
+        }
+    }
+    // bad implementation
+    public class Car {
+        public void drive(){
+            Console.Write("I carry people\n");
+            Console.Write("I carry cargo\n");
+            Console.Write("I carry money\n");
+        }
+    }
+
+
+    // good implementation
+    public class PassengerCar {
+        public void drive(){
+            Console.Write("I carry people\n");
+        }
+    }
+
+    public class Lorry {
+        public void drive(){
+            Console.Write("I carry cargo\n");
+        }
+    }
+
+    public class ArmoredTruck {
+        public void drive(){
+            Console.Write("I carry money\n");
+        }
+    }
+}
